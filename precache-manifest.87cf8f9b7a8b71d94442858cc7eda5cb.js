bb3f4984fa38e9c3cf77d749d542ad22cb35407a
yashraj15 <yashraj.jaiswal15@gmail.com>
2021-05-20 12:28:11 -0500

    Updates

---
+++ precache-manifest.87cf8f9b7a8b71d94442858cc7eda5cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8c58b40da3891e25a73",
+    "url": "/portfolio1/static/css/main.ce8532b7.chunk.css"
+  },
+  {
+    "revision": "e8c58b40da3891e25a73",
+    "url": "/portfolio1/static/js/main.e8c58b40.chunk.js"
+  },
+  {
+    "revision": "06eccff0c771d9748e7b",
+    "url": "/portfolio1/static/js/1.06eccff0.chunk.js"
+  },
+  {
+    "revision": "1116a28d5931134a0de4",
+    "url": "/portfolio1/static/js/runtime~main.1116a28d.js"
+  },
+  {
+    "revision": "23438ad5d627ba8fc99fc25fe9005332",
+    "url": "/portfolio1/index.html"
+  }
+];

90c51807514841da5108cac2d7081c18e47e601f
yashraj15 <yashraj.jaiswal15@gmail.com>
2021-05-25 17:04:07 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio1/precache-manifest.2cc134bf76b15ec58dcf88e0b9148034.js"
+  "/portfolio1/precache-manifest.48555c5f85aec4c9701304dffffbe992.js"
 );
 
 workbox.clientsClaim();
